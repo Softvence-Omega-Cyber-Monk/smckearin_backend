f4e20041b475bffb4201a89f316d2025232eaf24
shahadathhs <shahadathhossensajib732@gmail.com>
2025-12-22 12:20:39 +0600

    feat: shelter stats completed

--- src/main/stats/stats.controller.ts
+++ src/main/stats/stats.controller.ts
@@ -12,6 +12,7 @@ import { GraphFilterDto } from './dto/graph-filter.dto';
 import { AdminGraphStatsService } from './services/admin-graph-stats.service';
 import { AdminStatsService } from './services/admin-stats.service';
 import { DriverStatsService } from './services/driver-stats.service';
+import { ShelterGraphStatsService } from './services/shelter-graph-stats.service';
 import { ShelterStatsService } from './services/shelter-stats.service';
 import { VetGraphStatsService } from './services/vet-graph-stats.service';
 import { VetStatsService } from './services/vet-stats.service';
@@ -28,23 +29,24 @@ export class StatsController {
     private readonly shelterStatsService: ShelterStatsService,
     private readonly adminGraphStatsService: AdminGraphStatsService,
     private readonly vetGraphStatsService: VetGraphStatsService,
+    private readonly shelterGraphStatsService: ShelterGraphStatsService,
   ) {}
 
-  @ApiOperation({ summary: 'Get driver stats' })
+  @ApiOperation({ summary: 'Get driver stats (driver)' })
   @Get('driver/stats')
   @ValidateDriver()
   async getDriverStats(@GetUser('sub') userId: string) {
     return this.driverStatsService.getDriverStats(userId);
   }
 
-  @ApiOperation({ summary: 'Get vet stats' })
+  @ApiOperation({ summary: 'Get vet stats (vet)' })
   @Get('vet/stats')
   @ValidateVeterinarian()
   async getVetStats(@GetUser('sub') userId: string) {
     return this.vetStatsService.getVetStats(userId);
   }
 
-  @ApiOperation({ summary: 'Get vet pie chart stats' })
+  @ApiOperation({ summary: 'Get vet pie chart stats (vet)' })
   @Get('vet/stats/chart')
   @ValidateVeterinarian()
   async getCertificationOverview(@GetUser('sub') userId: string) {
@@ -58,31 +60,45 @@ export class StatsController {
     return this.vetGraphStatsService.getCertificationStats(userId);
   }
 
-  @ApiOperation({ summary: 'Get admin stats' })
+  @ApiOperation({ summary: 'Get admin stats (admin)' })
   @Get('admin/stats')
   @ValidateAdmin()
   async getAdminStats(@GetUser('sub') userId: string) {
     return this.adminStatsService.getAdminStats(userId);
   }
 
-  @ApiOperation({ summary: 'Get admin transport graph stats' })
+  @ApiOperation({ summary: 'Get admin transport graph stats (admin)' })
   @Get('admin/stats/graph')
   @ValidateAdmin()
   async getAdminGraphStats(@Query() filterDto: GraphFilterDto) {
     return this.adminGraphStatsService.getTransportGraph(filterDto);
   }
 
-  @ApiOperation({ summary: 'Get admin operational overview' })
+  @ApiOperation({ summary: 'Get admin operational overview stats (admin)' })
   @Get('admin/stats/overview')
   @ValidateAdmin()
   async getOperationalOverview(@Query() filterDto: GraphFilterDto) {
     return this.adminGraphStatsService.getOperationalOverview(filterDto);
   }
 
-  @ApiOperation({ summary: 'Get shelter stats' })
+  @ApiOperation({ summary: 'Get shelter stats (shelter)' })
   @Get('shelter/stats')
   @ValidateManager()
   async getShelterStats(@GetUser('sub') userId: string) {
     return this.shelterStatsService.getShelterStats(userId);
+  }
+
+  @ApiOperation({ summary: 'Get shelter trips overview stats (shelter)' })
+  @Get('shelter/stats/chart')
+  @ValidateManager()
+  async getTripsOverview(@GetUser('sub') userId: string) {
+    return this.shelterGraphStatsService.getTripsOverview(userId);
+  }
+
+  @ApiOperation({ summary: 'Get shelter trips stats (shelter)' })
+  @Get('shelter/stats/trips')
+  @ValidateManager()
+  async getTripsStats(@GetUser('sub') userId: string) {
+    return this.shelterGraphStatsService.getTripsStats(userId);
   }
 }
--- src/main/stats/stats.module.ts
+++ src/main/stats/stats.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AdminGraphStatsService } from './services/admin-graph-stats.service';
 import { AdminStatsService } from './services/admin-stats.service';
 import { DriverStatsService } from './services/driver-stats.service';
+import { ShelterGraphStatsService } from './services/shelter-graph-stats.service';
 import { ShelterStatsService } from './services/shelter-stats.service';
 import { VetGraphStatsService } from './services/vet-graph-stats.service';
 import { VetStatsService } from './services/vet-stats.service';
@@ -16,6 +17,7 @@ import { StatsController } from './stats.controller';
     DriverStatsService,
     VetStatsService,
     VetGraphStatsService,
+    ShelterGraphStatsService,
   ],
 })
 export class StatsModule {}
