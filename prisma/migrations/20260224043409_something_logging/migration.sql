90b725134a3abaab6b54d57d35e60c41f93f3ce2
Amitav <amitavrchy01@gmail.com>
2026-02-24 14:32:58 +0600

    feat: fixing prisma db error

--- prisma/migrations/20260224043409_something_logging/migration.sql
+++ prisma/migrations/20260224043409_something_logging/migration.sql
@@ -1,2 +1,9 @@
--- RenameIndex
-ALTER INDEX "private_conversations_initiatorId_receiverId_shelterId_chatScop" RENAME TO "private_conversations_initiatorId_receiverId_shelterId_chat_key";
+-- RenameIndex (idempotent)
+DO $$
+BEGIN
+  IF to_regclass('public.private_conversations_initiatorId_receiverId_shelterId_chatScop') IS NOT NULL
+     AND to_regclass('public.private_conversations_initiatorId_receiverId_shelterId_chat_key') IS NULL THEN
+    ALTER INDEX "private_conversations_initiatorId_receiverId_shelterId_chatScop"
+      RENAME TO "private_conversations_initiatorId_receiverId_shelterId_chat_key";
+  END IF;
+END $$;
